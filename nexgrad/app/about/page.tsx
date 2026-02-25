927d32b2c7c76464678fe836c45d8a79f7c8e4f4
hundal44 <147229206+KuldeepEdunova@users.noreply.github.com>
2026-02-25 14:38:19 +0530

    Delete page.tsx

--- nexgrad/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-// import AboutSection from "@/components/sections/AboutSection";
-
-// export default function AboutPage() {
-//   return (
-//     <main className="bg-[#f7f7f7] min-h-screen py-20">
-//       <AboutSection />
-//     </main>
-//   );
-// }
